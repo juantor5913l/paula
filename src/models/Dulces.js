ed674f1341418146cfaafc0048fdbababd2fc444
mauricio <malarcon@unicauca.edu.co>
2022-11-11 22:41:46 -0500

    first commit

---
+++ src/models/Dulces.js
@@ -0,0 +1,34 @@
+const mongo = require("mongoose"); 
+
+const DulcesSchema = new mongo.Schema(
+    {
+        referencia: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        nombre: {
+            type: String,
+            required: true
+        },
+        cantidad: {
+            type: String,
+            required: true
+        },
+        precio: {
+            type: String,
+            required: true
+        },
+        Descripcion: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true,
+        versionKey: false
+    }
+); 
+
+const Dulce = new mongo.model("Dulce", DulcesSchema);
+module.exports = Dulce;  
